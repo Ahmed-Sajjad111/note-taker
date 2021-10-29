b43d581eaacade12d015d8f3973ab8f9ac923ebf
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-10-29 09:43:10 -0400

    added post request to add nodes with unique ids and other API get requests

--- server.js
+++ server.js
@@ -1,25 +1,44 @@
-const express = require('express')
+const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const {nanoid} = require('nanoid')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+
+    req.body.id = nanoid()
+
+    notes.push(newNote)
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2))
+
+    res.json(notes)
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-})
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
+    console.log(`API server now on port ${PORT}!`);
 });
